4bba457441049fa0e50bf273727a20bbf7e9354e
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-04-08 15:22:26 +0300

    ANGULAR

---
+++ Goals/src/app/what-to-do.spec.ts
@@ -0,0 +1,7 @@
+import { WhatToDo } from './what-to-do';
+
+describe('WhatToDo', () => {
+  it('should create an instance', () => {
+    expect(new WhatToDo()).toBeTruthy();
+  });
+});
---
+++ Goals/src/app/what-to-do.ts
@@ -0,0 +1,4 @@
+export class WhatToDo {
+    id: number;
+    name: string;
+}
