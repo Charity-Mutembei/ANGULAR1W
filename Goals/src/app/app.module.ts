6dabf8c72705cd71466a4fa4b537778d4b381b06
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-04-08 16:16:39 +0300

    component

--- Goals/src/app/app.module.ts
+++ Goals/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WhatToDoComponent } from './what-to-do/what-to-do.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WhatToDoComponent
   ],
   imports: [
     BrowserModule,
